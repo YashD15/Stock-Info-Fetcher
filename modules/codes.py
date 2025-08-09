a313ceab1519463e07c1db89a041a7b33735acc5
Yash Dhavde <yashdhavde1511@gmail.com>
2025-08-09 11:59:30 +0530

    nse list

--- modules/codes.py
+++ modules/codes.py
@@ -2,3 +2,175 @@
 # nseCodes = ('IOC','ONGC','BEL','BHEL','RCF','ABSLAMC','BSE')
 
 nseCodes = ('IOC','BEL')
+
+# Nifty 50 (as of 9-8-25)
+nifty50 = (
+    "RELIANCE", "HDFCBANK", "BHARTIARTL", "TCS", "ICICIBANK", "SBIN", "INFY", "HINDUNILVR", "BAJFINANCE",
+    "ITC", "LT", "HCLTECH", "MARUTI", "M&M", "KOTAKBANK", "SUNPHARMA", "ULTRACEMCO", "AXISBANK", "NTPC",
+    "TITAN",
+    "BAJAJFINSV",
+    "ONGC",
+    "ADANIPORTS",
+    "BEL",       # Bharat Electronics
+    "GRASIM",
+    "JSWSTEEL",
+    "POWERGRID",
+    "DIVISLAB",
+    "NESTLEIND",
+    "TATACONSUM",
+    "BRITANNIA",
+    "DRREDDY",
+    "SBILIFE",
+    "TATAMOTORS",
+    "EICHERMOT",
+    "BPCL",
+    "HINDALCO",
+    "COALINDIA",
+    "APOLLOHOSP",
+    "CIPLA",
+    "TATASTEEL",
+    "HEROMOTOCO",
+    "HDFCLIFE",
+    "UPL",
+    "WIPRO",
+    "SHRIRAMFIN",
+    "ADANIENT",
+    "TECHM",
+    "ASIANPAINT",
+    "HDFC",
+    "JIOFIN",
+    "TRENT",
+    "ZOMATO"
+)
+
+niftynext50 = (
+    "ABB",               # ABB India
+    "ADANIENSOL",        # Adani Energy Solutions
+    "ADANIGREEN",        # Adani Green Energy
+    "ADANIPOWER",        # Adani Power
+    "AMBUJACEM",         # Ambuja Cements
+    "BAJAJHLDNG",        # Bajaj Holdings & Investment
+    "BAJAJHFL",          # Bajaj Housing Finance
+    "BANKBARODA",        # Bank of Baroda
+    "BPCL",              # Bharat Petroleum Corporation Ltd
+    "BRITANNIA",         # Britannia Industries
+    "BOSCHLTD",          # Bosch Ltd
+    "CANBK",             # Canara Bank
+    "CGPOWER",           # CG Power and Industrial Solutions
+    "CHOLAFIN",          # Cholamandalam Investment & Finance
+    "DABUR",             # Dabur India
+    "DIVISLAB",          # Divi's Laboratories
+    "DLF",               # DLF Ltd
+    "DMART",             # Avenue Supermarts (DMart)
+    "GAIL",              # GAIL (India) Ltd
+    "GODREJCP",          # Godrej Consumer Products
+    "HAVELLS",           # Havells India
+    "HAL",               # Hindustan Aeronautics Ltd
+    "HYUNDAI",           # Hyundai Motor India
+    "ICICIGI",           # ICICI Lombard General Insurance
+    "ICICIPRULI",        # ICICI Prudential Life Insurance
+    "INDHOTEL",          # Indian Hotels Company
+    "IOC",               # Indian Oil Corporation
+    "INDIGO",            # InterGlobe Aviation
+    "NAUKRI",            # Info Edge (India)
+    "IRFC",              # Indian Railway Finance Corporation
+    "JINDALSTEL",        # Jindal Steel & Power
+    "JSWENERGY",         # JSW Energy
+    "LICI",              # Life Insurance Corporation of India
+    "LODHA",             # Macrotech Developers (Lodha)
+    "LTIM",              # LTIMindtree Ltd
+    "PIDILITIND",        # Pidilite Industries
+    "PFC",               # Power Finance Corporation
+    "PNB",               # Punjab National Bank
+    "RECLTD",            # REC Ltd
+    "MOTHERSON",         # Samvardhana Motherson International
+    "SHREECEM",          # Shree Cement Ltd
+    "SIEMENS",           # Siemens Ltd
+    "SWIGGY",            # Swiggy
+    "TATAPOWER",         # Tata Power
+    "TORNTPHARM",        # Torrent Pharmaceuticals
+    "TVSMOTOR",          # TVS Motor Company
+    "UNITDSPR",          # United Spirits Ltd
+    "VBL",               # Varun Beverages Ltd
+    "VEDL",              # Vedanta Ltd
+    "ZYDUSLIFE"          # Zydus Lifesciences
+)
+
+# NIFTY 100
+nifty_100 = (
+    "RELIANCE", "HDFCBANK", "BHARTIARTL", "TCS", "ICICIBANK", "SBIN", "INFY", "HINDUNILVR", "BAJFINANCE", "ITC",
+    "LT", "HCLTECH", "MARUTI", "M&M", "KOTAKBANK", "SUNPHARMA", "ULTRACEMCO", "AXISBANK", "NTPC", "TITAN",
+    "BAJAJFINSV", "ONGC", "ADANIPORTS", "BEL", "GRASIM", "JSWSTEEL", "POWERGRID", "DIVISLAB", "NESTLEIND", "TATACONSUM",
+    "BRITANNIA", "DRREDDY", "SBILIFE", "TATAMOTORS", "EICHERMOT", "BPCL", "HINDALCO", "COALINDIA", "APOLLOHOSP", "CIPLA",
+    "TATASTEEL", "HEROMOTOCO", "HDFCLIFE", "UPL", "WIPRO", "SHRIRAMFIN", "ADANIENT", "TECHM", "ASIANPAINT", "HDFC",
+    "JIOFIN", "TRENT", "ZOMATO", "ABB", "ADANIENSOL", "ADANIGREEN", "ADANIPOWER", "AMBUJACEM", "BAJAJHLDNG",
+    "BAJAJHFL", "BANKBARODA", "BOSCHLTD", "CANBK", "CGPOWER", "CHOLAFIN", "DABUR", "DLF", "DMART", "GAIL",
+    "GODREJCP", "HAVELLS", "HAL", "HYUNDAI", "ICICIGI", "ICICIPRULI", "INDHOTEL", "IOC", "INDIGO", "NAUKRI",
+    "IRFC", "JINDALSTEL", "JSWENERGY", "LICI", "LODHA", "LTIM", "PIDILITIND", "PFC", "PNB", "RECLTD",
+    "MOTHERSON", "SHREECEM", "SIEMENS", "SWIGGY", "TATAPOWER", "TORNTPHARM", "TVSMOTOR", "UNITDSPR", "VBL", "VEDL",
+    "ZYDUSLIFE"
+)
+
+# NIFTY BANK
+nifty_bank = (
+    "SBIN", "ICICIBANK", "IDFCFIRSTB", "AUBANK", "AXISBANK", "BANDHANBNK",
+    "FEDERALBNK", "HDFCBANK", "INDUSINDBK", "KOTAKBANK", "PNB", "RBLBANK"
+)
+
+# NIFTY IT
+nifty_it = (
+    "TCS", "INFY", "HCLTECH", "WIPRO", "LTIM", "TECHM",
+    "PERSISTENT", "OFSS", "MPHASIS"
+)
+
+# NIFTY FMCG
+nifty_fmcg = (
+    "NESTLEIND", "PATANJALI", "EMAMILTD", "VBL", "UNITDSPR",
+    "UBL", "TATACONSUM", "RADICO", "MARICO", "ITC", "HINDUNILVR", "BRITANNIA"
+)
+
+# NIFTY AUTO
+nifty_auto = (
+    "TATAMOTORS", "BAJAJ_AUTO", "EICHERMOT", "TVSMOTOR", "BOSCHLTD", "MOTHERSON",
+    "EICHERMOT", "HEROMOTOCO"
+)
+
+# NIFTY METAL
+nifty_metal = (
+    "ADANIENT", "JSWSTEEL", "TATASTEEL", "HINDZINC", "VEDL",
+    "SAIL", "HINDCOPPER", "NMDC", "TATAMETALI", "COCHINSHIP",
+    "METROPOLIS", "ITC", "JINDALSTEL", "WELCORP", "CASTROLIND"
+)
+
+# NIFTY PHARMA
+nifty_pharma = (
+    "LUPIN", "ABBOTINDIA", "AUROPHARMA", "CIPLA", "DRREDDY", "GLENMARK",
+    "DIVISLAB", "CADILAHC", "ALKEM", "SANOFI", "BIOSPHERE", "TATAPHARM"
+)
+
+# Nifty Midcap Select (25 companies)
+nifty_midcap_select = (
+    "MAXHEALTH", "HDFCAMC", "CUMMINSIND", "POLYCAB", "BSE", "DIXON", "MARICO",
+    "INDUSTOW", "LUPIN", "HINDPETRO", "SRF", "PERSISTENT", "BHARATFORG", "YESBANK",
+    "COFORGE", "MPHASIS", "VOLTAS", "MUTHOOTFIN", "AUROPHARMA", "COLPAL",
+    "GODREJPROP", "ASHOKLEY", "BAJAJHLDNG", "GLENMARK", "DRREDDY"
+)
+
+# Nifty Midcap 150 (150 companies, partial list shown here)
+nifty_midcap_150 = (
+    "HINDZINC", "SOLARINDS", "MAXHEALTH", "HDFCAMC", "MAZDOCK", "CUMMINSIND", "MUTHOOTFIN",
+    "MANKINDPHA", "POLYCAB", "UNIONBANK", "BSE", "DIXON", "GMRINFRA", "MARICO",
+    "HITACHIELE", "INDUSTOW", "WAAREE", "BHARATHEX", "INDIAN", "LUPIN", "HINDPETRO",
+    "SUZLON", "NTPC", "SRF", "NHPC", "DABUR", "HAVELLS", "DMART",
+    "GODREJCP", "PIDILITIND", "API", "BERGEPAINT", "NESTLEIND", "MAHABANK",
+    "BAJAJHFL", "ESCORTS", "PIDILITIND", "MINDTREE", "JINDALSTEL", "BANKBARODA",
+    "TECHM", "CANBK", "EICHERMOT", "ICICIGI", "AUROPHARMA", "TATASTEEL", "TORNTPHARM",
+    "LICI", "OBEROIRLTY", "LTI", "BALRAMCHIN", "GMRINFRA", "DEEPAKFERT", "BRAHMAPUTRA",
+    "GAIL", "PIDILITIND", "COLPAL", "MCDOWELL_N", "ADANIPORTS", "LUPIN", "MOTHERSUMI",
+    "BRIGADE", "TEJASNET", "MCLEODRUSS", "MOIL", "AUROPHARMA", "EXIDEIND", "LTI",
+    "CRISIL", "BALKRISHNA", "ABBOTINDIA", "ESCORTS", "VOLTAS", "COLPAL", "SUZLON",
+    "DABUR", "PIDILITIND", "INDIGO", "HUDCO", "LUPIN", "TATASTEEL", "SRF", "HDFCAMC",
+    "CAPLIPOINT", "MUTHOOTFIN", "ASHOKLEY", "HINDUNILVR", "HINDPETRO", "M&M", "HINDALCO",
+    "VOLTAS"
+    # Note: This is a partial list for illustration; the full list contains 150 companies.
+)
